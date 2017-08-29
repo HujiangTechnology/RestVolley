4fbe31eacfbb84d7ecd16958be1ffef7619f869a
firefly1126 <xiaoming1109@gmail.com>
2017-08-29 16:22:46 +0800

    fix: protocol exception

--- restvolley/src/main/java/com/hujiang/restvolley/OkHttpStack.java
+++ restvolley/src/main/java/com/hujiang/restvolley/OkHttpStack.java
@@ -94,7 +94,6 @@ public class OkHttpStack implements HttpStack {
             connection.addRequestProperty(headerName, map.get(headerName));
         }
         setConnectionParametersForRequest(connection, request);
-        connection.connect();
         // Initialize HttpResponse with data from the HttpURLConnection.
         ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 1);
         int responseCode = connection.getResponseCode();
